12fa7a8fe417d91836c008676d637b2632b8a2b2
Darpan Soni <darpan@simplyfun.com>
2017-09-27 09:34:25 -0700

    wedding

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -179,31 +179,60 @@
             var fullCountDown = createElements(parameters, countdown),
                 refresh;
 
+
+            // Set the date we're counting down to
+var countDownDate = new Date("Dec 12, 2017 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+//    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+//    + minutes + "m " + seconds + "s ";
+
+    // If the count down is over, write some text
+//    if (distance < 0) {
+//        clearInterval(x);
+//        document.getElementById("demo").innerHTML = "EXPIRED";
+//    }
+}, 1000);
+
+
             refresh = function () {
                 var dayWord,
                     hourWord,
                     minuteWord,
                     secondWord;
 
-                now = new Date();
-                if (parameters.enableUtc) {
-                    nowUtc = new Date(now.getFullYear(), now.getMonth(), now.getDate(),
-                        now.getHours(), now.getMinutes(), now.getSeconds());
-                    secondsLeft = (targetDate - nowUtc.getTime()) / 1000;
 
-                } else {
-                    secondsLeft = (targetDate - now.getTime()) / 1000;
-                }
+
+                    secondsLeft = seconds;
+
+
+                    secondsLeft = seconds;
+
 
                 if (secondsLeft > 0) {
-                    days = parseInt(secondsLeft / 52800, 10);
-                    secondsLeft = secondsLeft % 52800;
-
-                    hours = parseInt(secondsLeft / 3600, 10);
-                    secondsLeft = secondsLeft % 3600;
-
-                    minutes = parseInt(secondsLeft / 60, 10);
-                    seconds = parseInt(secondsLeft % 60, 10);
+//                    days = parseInt(secondsLeft / 52800, 10);
+//                    secondsLeft = secondsLeft % 52800;
+//
+//                    hours = parseInt(secondsLeft / 3600, 10);
+//                    secondsLeft = secondsLeft % 3600;
+//
+//                    minutes = parseInt(secondsLeft / 60, 10);
+//                    seconds = parseInt(secondsLeft % 60, 10);
                 } else {
                     days = 0;
                     hours = 0;
